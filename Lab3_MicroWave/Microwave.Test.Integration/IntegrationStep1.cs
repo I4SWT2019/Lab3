500e34333ef9627ac3d8a72be976f526760feacb
Morten Filt <201600311@post.au.dk>
2019-11-23 22:35:46 +0100

    Added draft of Integration test

---
+++ Lab3_MicroWave/Microwave.Test.Integration/IntegrationStep1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    // 
+    [TestFixture]
+    [Author("I4SWTGrp2")]
+    public class IntegrationStep1
+    {
+
+        private UserInterface userInterface;
+        private Door door;
+        private Light light;
+
+        private IButton powerButton;
+        private IButton timeButton;
+        private IButton startCancelButton;
+
+        private IDisplay display;
+
+        private ICookController cooker;
+
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            powerButton = Substitute.For<IButton>();
+            timeButton = Substitute.For<IButton>();
+            startCancelButton = Substitute.For<IButton>();
+            display = Substitute.For<IDisplay>();
+            cooker = Substitute.For<ICookController>();
+            output = Substitute.For<IOutput>();
+
+            door = new Door();
+            light = new Light(output);
+
+            userInterface = new UserInterface(
+                powerButton, timeButton, startCancelButton,
+                door,
+                display,
+                light,
+                cooker);
+        }
+
+        [Test]
+        public void DoorOpen_LightOne()
+        {
+            door.Open();
+            output.Received(1).OutputLine("Light is turned on");
+        }
+
+        [Test]
+        public void DoorClosed_LightOff()
+        {
+            door.Open();
+            door.Close();
+            output.Received(1).OutputLine("Light is turned off");
+        }
+    }
+}
---
+++ Lab3_MicroWave/Microwave.Test.Integration/IntegrationStep2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IntegrationStep2
+    {
+        private UserInterface userInterface;
+        private Light light;
+        private Door door;
+
+        private Button powerButton;
+        private Button timeButton;
+        private Button startCancelButton;
+
+
+        private IDisplay display;
+
+        private ICookController cooker;
+
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            cooker = Substitute.For<ICookController>();
+            output = Substitute.For<IOutput>();
+            display = Substitute.For<IDisplay>();
+
+            powerButton = new Button();
+            timeButton = new Button();
+            startCancelButton = new Button();
+
+            door = new Door();
+
+            light = new Light(output);
+
+            userInterface = new UserInterface(
+                powerButton, timeButton, startCancelButton,
+                door,
+                display,
+                light,
+                cooker);
+        }
+
+        [Test]
+        public void SetPower__DoorOpened_LightOn()
+        {
+            powerButton.Press();
+            door.Open();
+
+            output.Received(1).OutputLine("Light is turned on");
+        }
+    }
+}
